c8c70d833299b751a039702dbf3779cddfa67f5a
ravindrajangid00007 <ravindrajangid00007@gmail.com>
2021-06-03 13:06:00 +0530

    portfolio media query

--- script.js
+++ script.js
@@ -1,86 +1,86 @@
 'use strict'
-{
-
-    // let anchors = document.querySelectorAll('.nav-menu a');
-    // let id;
-    // for (let anchor of anchors) {
-    //     anchor.addEventListener('click', function (event) {
-    //         event.preventDefault();
-    //     }
-    //     );
-    // }
-    // for (let i = 0; i < anchors.length - 1; i++) {
-    //     anchors[i].addEventListener('click', function () {
-    //         let sl = this.attributes[0].textContent.length;
-    //         let targetSectionID = this.attributes[0].textContent.slice(1, sl,);
-    //         let targetSection = document.getElementById(targetSectionID);
-    //         if (targetSection) {
-
-    //             id = setInterval(scrollVertically, 1, targetSection);
-    //         }
-    //     });
-    // }
-    // var contactSection = document.getElementById('contact');
-    // anchors[anchors.length - 1].addEventListener('click', function () {
-    //     var contactID = setInterval(function () {
-    //         if (contactSection.getBoundingClientRect().top <= window.innerHeight - 300) {
-    //             clearInterval(contactID);
-    //             return;
-    //         }
-    //         window.scrollBy(0, 50);
-    //     }, 1);
-    // });
 
 
-    // function scrollVertically(targetSection) {
-    //     let targetDestination = targetSection.getBoundingClientRect().top;
-    //     if (targetDestination <= 50) {
-    //         clearInterval(id);
-    //         return;
-    //     }
-    //     window.scrollBy(0, 50);
-    // }
+// let anchors = document.querySelectorAll('.nav-menu a');
+// let id;
+// for (let anchor of anchors) {
+//     anchor.addEventListener('click', function (event) {
+//         event.preventDefault();
+//     }
+//     );
+// }
+// for (let i = 0; i < anchors.length - 1; i++) {
+//     anchors[i].addEventListener('click', function () {
+//         let sl = this.attributes[0].textContent.length;
+//         let targetSectionID = this.attributes[0].textContent.slice(1, sl,);
+//         let targetSection = document.getElementById(targetSectionID);
+//         if (targetSection) {
+
+//             id = setInterval(scrollVertically, 1, targetSection);
+//         }
+//     });
+// }
+// var contactSection = document.getElementById('contact');
+// anchors[anchors.length - 1].addEventListener('click', function () {
+//     var contactID = setInterval(function () {
+//         if (contactSection.getBoundingClientRect().top <= window.innerHeight - 300) {
+//             clearInterval(contactID);
+//             return;
+//         }
+//         window.scrollBy(0, 50);
+//     }, 1);
+// });
 
 
+// function scrollVertically(targetSection) {
+//     let targetDestination = targetSection.getBoundingClientRect().top;
+//     if (targetDestination <= 50) {
+//         clearInterval(id);
+//         return;
+//     }
+//     window.scrollBy(0, 50);
+// }
 
 
 
 
 
 
-    // skill- bar 
-    let bars = document.getElementsByClassName("block2");
-    let skillSection = document.getElementById('skills');
 
-    function initilizeBarsWidth() {
-        for (let bar of bars) {
-            bar.style.width = 0 + '%';
-        }
+
+// skill- bar 
+let bars = document.getElementsByClassName("block2");
+let skillSection = document.getElementById('skills');
+
+function initilizeBarsWidth() {
+    for (let bar of bars) {
+        bar.style.width = 0 + '%';
     }
-    initilizeBarsWidth();
-    let animationDone = false;
-    function setColorAnimation() {
-        if (!animationDone && skillSection.getBoundingClientRect().top <= window.innerHeight - 600) {
-            animationDone = true;
-            for (let bar of bars) {
-                let skillBar = bar.attributes[1].value;
-                // console.log(skillBar);
-                let currentWidth = 0;
-                let barID = setInterval(function () {
-                    if (currentWidth >= skillBar) {
-                        clearInterval(barID);
-                        return;
-                    }
-                    bar.style.width = currentWidth + "%";
-                    currentWidth++;
-                }, 5);
-            }
-            console.log("hello");
-        }
-    }
-
-    window.addEventListener('scroll', setColorAnimation);
 }
+initilizeBarsWidth();
+let animationDone = false;
+function setColorAnimation() {
+    if (!animationDone && skillSection.getBoundingClientRect().top <= window.innerHeight - 600) {
+        animationDone = true;
+        for (let bar of bars) {
+            let skillBar = bar.attributes[1].value;
+            // console.log(skillBar);
+            let currentWidth = 0;
+            let barID = setInterval(function () {
+                if (currentWidth >= skillBar) {
+                    clearInterval(barID);
+                    return;
+                }
+                bar.style.width = currentWidth + "%";
+                currentWidth++;
+            }, 5);
+        }
+        console.log("hello");
+    }
+}
+
+window.addEventListener('scroll', setColorAnimation);
+
 
 
 // project-BhxBrowser
@@ -90,34 +90,72 @@ var back = document.getElementsByClassName("back");
 for (var i = 0; i < back.length; i++) {
 
     back[i].addEventListener("mouseenter", function () {
-        if (window.innerWidth > 650) {
+        if (window.innerWidth >= 900) {
             this.children[1].style.top = "50px";
             this.children[1].style.left = "-75px";
             this.children[1].style.height = "150px";
             this.children[1].style.width = "150px";
             this.children[1].style.backgroundSize = "150px 150px";
         } else {
-            // this.children[1].style.transform = "rotateY(90deg)";
-            this.children[1].style.width = "0px";
-            this.children[1].style.backgroundSize = "0px 250px";
+
+            if (window.innerWidth > 610) {
+                this.children[1].style.top = "40px";
+                this.children[1].style.left = "-60px";
+                this.children[1].style.height = "120px";
+                this.children[1].style.width = "120px";
+                this.children[1].style.backgroundSize = "120px 120px";
+            } else {
+                // this.children[1].style.transform = "rotateY(90deg)";
+                this.children[1].style.width = "0px";
+                this.children[1].style.backgroundSize = "0px 200px";
+            }
         }
+
     });
     back[i].addEventListener("mouseleave", function () {
-        if (window.innerWidth > 650) {
+        if (window.innerWidth >= 900) {
             this.children[1].style.top = "0px";
             this.children[1].style.left = "0px";
             this.children[1].style.height = "250px";
             this.children[1].style.width = "450px";
             this.children[1].style.backgroundSize = "450px 250px";
         } else {
-            
-            // this.children[1].style.transform = "rotateY(0deg)";
-            this.children[1].style.backgroundSize = "450px 250px";
-            this.children[1].style.width = "450px";
+            if (window.innerWidth > 610){
+                this.children[1].style.top = "0px";
+                this.children[1].style.left = "0px";
+                this.children[1].style.height = "200px";
+                this.children[1].style.width = "400px";
+                this.children[1].style.backgroundSize = "400px 200px";
+            } else {
+    
+                // this.children[1].style.transform = "rotateY(0deg)";
+                this.children[1].style.backgroundSize = "400px 200px";
+                this.children[1].style.width = "400px";
+            }
         }
+        
 
 
     });
 }
+
+
+window.addEventListener('scroll', function () {
+    for (let obj of back) {
+        if (window.innerWidth >= 900) {
+            obj.children[1].style.top = "0px";
+            obj.children[1].style.left = "0px";
+            obj.children[1].style.height = "250px";
+            obj.children[1].style.width = "450px";
+            obj.children[1].style.backgroundSize = "450px 250px";
+        } else {
+            obj.children[1].style.top = "0px";
+            obj.children[1].style.left = "0px";
+            obj.children[1].style.height = "200px";
+            obj.children[1].style.width = "400px";
+            obj.children[1].style.backgroundSize = "400px 200px";
+        }
+    }
+});
 
 
