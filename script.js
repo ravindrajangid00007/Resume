26a13c0df1f4c601dd07bb6173e8d4992a6d8794
ravindrajangid00007 <ravindrajangid00007@gmail.com>
2021-06-04 11:14:46 +0530

    border under nav  setted and  portfolio size bug removed

--- script.js
+++ script.js
@@ -85,41 +85,69 @@ window.addEventListener('scroll', setColorAnimation);
 
 // project-BhxBrowser
 
+var upper = document.getElementsByClassName("upper");
 var back = document.getElementsByClassName("back");
-
-for (var i = 0; i < back.length; i++) {
-
-    back[i].addEventListener("mouseenter", function () {
-        if (window.innerWidth >= 550) {
-            this.children[0].style.top = "40px";
-            this.children[0].style.left = "-60px";
-            this.children[0].style.height = "120px";
-            this.children[0].style.width = "120px";
-            this.children[0].style.backgroundSize = "120px 120px";
-        }
-
-    });
-    back[i].addEventListener("mouseleave", function () {
-        if (window.innerWidth >= 550) {
-            this.children[0].style.top = "0px";
-            this.children[0].style.left = "0px";
-            this.children[0].style.height = "200px";
-            this.children[0].style.width = "400px";
-            this.children[0].style.backgroundSize = "400px 200px";
-        }
-
-
-
-    });
+function upperChangeSize(){
+    this.children[0].classList.toggle('sizeofupper');
+}
+for(var obj of back){
+    obj.addEventListener("mouseenter", upperChangeSize);
+    obj.addEventListener("mouseleave" , upperChangeSize)
 }
 
+
+// ham burger menu 
 
 
 var hamBurger = document.querySelector('.mobile-menu') ;
 var navList = document.querySelector('.nav-menu-list') ;
+
 function activeContainer(){
     navList.classList.toggle('active');
 }
 hamBurger.addEventListener('click' , activeContainer)
 
 
+// highlight border
+var element = document.querySelector('.highlight');
+var homePage = document.querySelector('#home-page');
+var aboutPage = document.querySelector('#about-page');
+var skillPage = document.querySelector('#skills-page');
+var experiencePage = document.querySelector('#experience-page');
+var  educationPage= document.querySelector('#education-page');
+var portfolioPage = document.querySelector('#portfolio-page');
+var contactPage = document.querySelector('#contact-page');
+
+function src() {
+
+    var pos = window.scrollY;
+    if (window.innerWidth > 1050 && pos < 306) {
+        aboutPage.classList.remove('highlight');
+    } else if (window.innerWidth > 1050 && pos < 745) {
+        aboutPage.classList.add('highlight');
+        skillPage.classList.remove('highlight');
+    } else if (window.innerWidth > 1050 && pos < 1459) {
+        skillPage.classList.add('highlight');
+        aboutPage.classList.remove('highlight');
+        experiencePage.classList.remove('highlight') ;
+    } else if (window.innerWidth > 1050 && pos < 2581){
+        experiencePage.classList.add('highlight') ;
+        skillPage.classList.remove('highlight');
+        educationPage.classList.remove('highlight');
+    } else if (window.innerWidth > 1050 && pos < 3499){
+        educationPage.classList.add('highlight');
+        experiencePage.classList.remove('highlight') ;
+        portfolioPage.classList.remove('highlight');
+    } else if (window.innerWidth > 1050 && pos < 4159){
+        portfolioPage.classList.add('highlight');
+        educationPage.classList.remove('highlight');
+        contactPage.classList.remove('highlight') ;
+    } else {
+        contactPage.classList.add('highlight');
+        portfolioPage.classList.remove('highlight') ;
+    }
+}
+
+
+setInterval(src, 10);
+window.addEventListener('click', src);
